92687155dd9480e4a6eec36091f5014120492220
Myintzu28 <myintzukhin28@gmail.com>
2024-03-06 09:47:19 +0630

    get all employees

--- src/main/java/com/spring/POS/services/EmployeeService.java
+++ src/main/java/com/spring/POS/services/EmployeeService.java
@@ -2,8 +2,12 @@ package com.spring.POS.services;
 
 import com.spring.POS.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
 
     EmployeeDto getEmployeeById(Long employeeId);
+
+    List<EmployeeDto> getAllEmployees();
 }
--- src/main/java/com/spring/POS/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/spring/POS/services/impl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.spring.POS.services.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -33,5 +36,12 @@ public class EmployeeServiceImpl implements EmployeeService {
                         new ResourceNotFoundException("Employee is not exist with given id :" + employeeId));
 
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
