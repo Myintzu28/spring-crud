4541bb638905d49291d70d2d4c7637ca0db665e2
Myintzu28 <myintzukhin28@gmail.com>
2024-03-06 09:04:17 +0630

    get employee

--- src/main/java/com/spring/POS/services/impl/EmployeeServiceImpl.java
+++ src/main/java/com/spring/POS/services/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.spring.POS.services.impl;
 
 import com.spring.POS.dto.EmployeeDto;
+import com.spring.POS.exception.ResourceNotFoundException;
 import com.spring.POS.mapper.EmployeeMapper;
 import com.spring.POS.models.Employee;
 import com.spring.POS.repo.EmployeeRepository;
@@ -13,10 +14,24 @@ import org.springframework.stereotype.Service;
 public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
+
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        if (employeeDto.getEmail() == null || employeeDto.getEmail().isEmpty()) {
+            throw new IllegalArgumentException("Email is required.");
+        }
+
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException("Employee is not exist with given id :" + employeeId));
+
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
